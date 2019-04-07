1fa7e7bcd389d2963f0524ed1bebcd5d7065f31c
Canonip <uueba@student.kit.edu>
2019-04-07 17:19:28 +0200

    Changed hardcoded Bintypes to dynamic ones.

--- node_helper.js
+++ node_helper.js
@@ -5,13 +5,13 @@ var moment = require("moment");
 
 module.exports = NodeHelper.create({
 
-  start: function() {
+  start: function () {
     console.log("Starting node_helper for module: " + this.name);
     this.schedule = null;
     this.garbageScheduleCSVFile = this.path + "/garbage_schedule.csv";
   },
 
-  socketNotificationReceived: function(notification, payload) {
+  socketNotificationReceived: function (notification, payload) {
 
     var self = this;
 
@@ -20,9 +20,9 @@ module.exports = NodeHelper.create({
 
       var scheduleFile = this.garbageScheduleCSVFile;
 
-      fs.readFile(scheduleFile, "utf8", function(err, rawData) {
+      fs.readFile(scheduleFile, "utf8", function (err, rawData) {
         if (err) throw err;
-        parse(rawData, {delimiter: ",", columns: true, ltrim: true}, function(err, parsedData) {
+        parse(rawData, { delimiter: ",", columns: true, ltrim: true }, function (err, parsedData) {
           if (err) throw err;
 
           self.schedule = parsedData;
@@ -36,29 +36,27 @@ module.exports = NodeHelper.create({
 
   },
 
-  postProcessSchedule: function() {
-
-    this.schedule.forEach( function(obj) {
-
-      //Convert date strings to moment.js Date objects
-      obj.pickupDate = moment(obj.WeekStarting, "MM/DD/YY");
-
-      //Reassign strings to booleans for particular waste type
-      obj.GreenBin = (obj.GreenBin == "0" ? false : true);
-      obj.GarbageBin = (obj.GarbageBin == "0" ? false : true);
-      obj.PaperBin = (obj.PaperBin == "0" ? false : true);
+  postProcessSchedule: function () {
+    this.schedule.forEach(function (obj) {
+      for (var key in obj) {
+        //Convert date strings to moment.js Date objects
+        if (key == "WeekStarting")
+          obj.pickupDate = moment(obj.WeekStarting, "MM/DD/YY");
+        //Reassign strings to booleans for particular waste type
+        else if (key != "WeekStarting" && key != "pickupDate")
+          obj[key] = obj[key] !== "0";
+      }
     });
-
   },
 
-  getNextPickups: function(payload) {
+  getNextPickups: function (payload) {
     var start = moment().startOf("day"); //today, 12:00 AM
     var end = moment().startOf("day").add(payload.weeksToDisplay * 7, "days");
 
     //find info for next pickup dates
     var nextPickups = this.schedule.filter(function (obj) {
       return obj.Calendar == payload.collectionCalendar &&
-        obj.pickupDate.isSameOrAfter(start) && 
+        obj.pickupDate.isSameOrAfter(start) &&
         obj.pickupDate.isBefore(end);
     });
 
